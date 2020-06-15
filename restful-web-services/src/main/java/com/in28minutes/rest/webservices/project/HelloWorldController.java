cb44877ae69b0ae7b2de5e4309cb80c1b4ec305d
Arsene Kevin <kevinkouomeu@icloud.com>
2020-06-16 01:08:26 +0200

    hello world service with path variable

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/project/HelloWorldController.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/project/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.in28minutes.rest.webservices.project;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,6 +14,11 @@ public class HelloWorldController {
 
 	@GetMapping(path = "/hello-world-bean")
 	public HelloWorldBean helloWorldBean() {
-		return new HelloWorldBean("Hello Kevin");
+		return new HelloWorldBean("Hello World");
+	}
+
+	@GetMapping(path = "/hello-world/param/{name}")
+	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+		return new HelloWorldBean(String.format("Hello world, %s", name));
 	}
 }
