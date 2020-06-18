81dc40e43d7740bda47f6f0bc9035e0d8836abfd
Arsene Kevin <kevinkouomeu@icloud.com>
2020-06-18 22:32:44 +0200

    Implementing Validations for RESTful Services

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/project/user/UserDaoService.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/project/user/UserDaoService.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Component;
 public class UserDaoService {
 
 	private static List<User> users = new ArrayList<>();
-	private static int userCount = 5;
 
 	static {
 		users.add(new User(1, "Adam", new Date()));
@@ -21,13 +20,16 @@ public class UserDaoService {
 		users.add(new User(5, "Marie", new Date()));
 	}
 
+	private static int userCount = users.size();
+
 	public List<User> findAll() {
 		return users;
 	}
 
 	public User save(User user) {
 		if (user.getId() == null) {
-			user.setId(userCount++);
+			userCount++;
+			user.setId(userCount);
 		}
 		users.add(user);
 		return user;
