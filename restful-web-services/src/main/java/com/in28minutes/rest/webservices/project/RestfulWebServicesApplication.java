0852243c42f90c6ffb324ced22a86c50663dbd51
Arsene Kevin <kevinkouomeu@icloud.com>
2020-06-19 11:49:01 +0200

    Part 2 - Internationalization

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/project/RestfulWebServicesApplication.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/project/RestfulWebServicesApplication.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 
 @SpringBootApplication
 @ComponentScan
@@ -20,7 +20,7 @@ public class RestfulWebServicesApplication {
 
 	@Bean
 	public LocaleResolver localeResolver() {
-		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
 		localeResolver.setDefaultLocale(Locale.US);
 
 		return localeResolver;
