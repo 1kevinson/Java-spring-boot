46ea32d13a97d25cf3c6dbd88da7d852f442782b
Arsene Kevin <kevinkouomeu@icloud.com>
2020-06-19 13:11:19 +0200

    Configuring Auto Generation of Swagger Documentation

---
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/project/SwaggerConfig.java
@@ -0,0 +1,38 @@
+package com.in28minutes.rest.webservices.project;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.hateoas.client.LinkDiscoverer;
+import org.springframework.hateoas.client.LinkDiscoverers;
+import org.springframework.hateoas.mediatype.collectionjson.CollectionJsonLinkDiscoverer;
+import org.springframework.plugin.core.SimplePluginRegistry;
+
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+	@Primary
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2);
+	}
+
+	// Add this block in case of @Bean error , set this primary
+	@Bean
+	public LinkDiscoverers discoverers() {
+		List<LinkDiscoverer> plugins = new ArrayList<>();
+		plugins.add(new CollectionJsonLinkDiscoverer());
+		return new LinkDiscoverers(SimplePluginRegistry.create(plugins));
+	}
+
+	// Verify the api docs -> http://localhost:8080/v2/api-docs
+	// Go to ui -> http://localhost:8080/swagger-ui.html
+}
