c93e5411e97c81b7af83aea9ef0da1736e350346
Arsene Kevin <kevinkouomeu@icloud.com>
2020-06-19 10:16:37 +0200

    Internationalization for RESTful Services

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/project/RestfulWebServicesApplication.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/project/RestfulWebServicesApplication.java
@@ -1,8 +1,14 @@
 package com.in28minutes.rest.webservices.project;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @SpringBootApplication
 @ComponentScan
@@ -10,6 +16,22 @@ public class RestfulWebServicesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestfulWebServicesApplication.class, args);
+	}
+
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+
+		return localeResolver;
+	}
+
+	@Bean
+	public ResourceBundleMessageSource bundleMessageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("messages");
+
+		return messageSource;
 	}
 
 }
