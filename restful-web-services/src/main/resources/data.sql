287decf903d145362beb16572976717d65908eb8
Arsene Kevin <kevinkouomeu@icloud.com>
2020-07-02 12:52:47 +0200

    Creating Post Entity and Many to One Relationship with User Entity

--- restful-web-services/src/main/resources/data.sql
+++ restful-web-services/src/main/resources/data.sql
@@ -1,3 +1,12 @@
-insert into user values(10000, sysdate(), 'AB');
-insert into user values(20000, sysdate(), 'Jill');
-insert into user values(30000, sysdate(), 'Jam');
+insert into user values(1001, sysdate(), 'AB');
+insert into user values(1002, sysdate(), 'Jill');
+insert into user values(1003, sysdate(), 'Jam');
+
+insert into post values(10001, 'My first post', 1001);
+insert into post values(10002, 'My second post', 1001);
+insert into post values(10003, 'My third post', 1001);
+
+insert into post values(10004, 'My reddit post', 1002);
+
+insert into post values(10005, 'My twitter post', 1003);
+insert into post values(10006, 'My tweet ads post', 1003);
