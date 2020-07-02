9fdc1a558050d5e68beb5321e7644c13231a274f
Arsene Kevin <kevinkouomeu@icloud.com>
2020-07-02 09:53:57 +0200

    Defining a Service to manage the Entity - UserService and EntityManager

--- jpa-in-10-steps/src/main/java/com/in28minutes/learning/jpa/jpain10steps/entity/User.java
+++ jpa-in-10-steps/src/main/java/com/in28minutes/learning/jpa/jpain10steps/entity/User.java
@@ -1,9 +1,18 @@
 package com.in28minutes.learning.jpa.jpain10steps.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class User {
 
+	@Id
+	@GeneratedValue
 	private long id;
+
 	private String name;
+
 	private String role;
 
 	// JPA expects default contructor
---
+++ jpa-in-10-steps/src/main/java/com/in28minutes/learning/jpa/jpain10steps/service/UserDaoService.java
@@ -0,0 +1,18 @@
+package com.in28minutes.learning.jpa.jpain10steps.service;
+
+import javax.persistence.EntityManager;
+
+import org.springframework.stereotype.Repository;
+
+import com.in28minutes.learning.jpa.jpain10steps.entity.User;
+
+@Repository
+public class UserDaoService {
+
+	private EntityManager entityManager;
+
+	public long insert(User user) {
+		entityManager.persist(user);
+		return user.getId();
+	}
+}
